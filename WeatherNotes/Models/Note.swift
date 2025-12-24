184c734dd378810a547fb6ee8a79b9b687dd12dc
Maksim Kosharny <maksimkosharnyjj@gmail.com>
2025-12-24 14:32:42 +0800

    Initial Commit

---
+++ WeatherNotes/Models/Note.swift
@@ -0,0 +1,8 @@
+//
+//  Note.swift
+//  WeatherNotes
+//
+//  Created by Maksim Kosharny on 24.12.2025.
+//
+
+import Foundation
---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Maksim Kosharny on 24.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
